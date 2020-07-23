99dd488e9c705d37cb0057581afac84e215698ef
Laura Cozzi <laura.cozzi.ribeiro@gmail.com>
2020-07-23 15:09:41 -0300

    Possible moves of a piece

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,6 +1,6 @@
 package boardgame;
 
-public class Piece {
+public abstract class Piece {
 	
 	protected Position position; // não será visivel na camada do xadrez
 	private Board board;
@@ -14,7 +14,21 @@ public class Piece {
 		return board;
 	}
 
+	public abstract boolean[][] possibleMoves();
 	
+	public boolean possibleMove(Position position) {
+		return possibleMoves()[position.getRow()][position.getColumn()];
+	}
 	
-
+	//Ver se a peça possui pelo menos 1 movimento possível
+	public boolean isThereAnyPossibleMove() {
+		boolean[][] mat = possibleMoves();
+		for(int i = 0; i < mat.length; i++) {
+			for(int j = 0; j < mat.length; j++) {
+				if(mat[i][j])
+					return true;
+			}
+		}
+		return false;
+	}
 }
